8cae9a5a72abc9facaac2b8b1a4e190c63feb241
Hamim <hamimsiddiq915@gmail.com>
2026-06-28 20:59:38 +0600

    nested layout

---
+++ app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProductDetailsLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+    <>
+        {children}
+        <h1>Product Details Layout</h1>
+    </>
+);
+}
