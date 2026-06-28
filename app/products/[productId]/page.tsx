098eaf702236495ead7d7ac379baf2751b9de446
Hamim <hamimsiddiq915@gmail.com>
2026-06-28 22:31:54 +0600

    dynamic routing metadata

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,8 +1,25 @@
+import { Metadata } from "next";
+import { resolve } from "path";
+
+type Props = {
+  params: Promise<{productId: string}>;
+};
+
+export const generateMetadata = async ({params}: Props) : Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${id}`);
+    }, 1000);
+  })
+  return {
+    title: `Product ${title}`
+  }
+}
+
 export default async function ProductDetails({
   params,
-}: {
-  params: Promise<{ productId: string }>;
-}) {
+}: Props) {
   const { productId } = await params;
 
   return (
