28ca5fff61a59a28b08beeee923cf19b386c6ee7
Hamim <hamimsiddiq915@gmail.com>
2026-07-01 22:02:12 +0600

    recovering from error

--- app/products/[productId]/reviews/[reviewId]/error.tsx
+++ app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,26 @@
 "use client";
 
-export default function ReviewError({error}: {error: Error}) {
-    return <h1>Error: {error.message}</h1>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function ReviewError({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+    const router = useRouter();
+    const reload = () => {
+        startTransition(() => {
+            router.refresh();
+            reset();
+        });
+    }
+  return (
+    <div>
+      <h1>Error: {error.message}</h1>
+      <button onClick={reload}>Try Again</button>
+    </div>
+  );
 }
