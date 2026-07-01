d649d69f44e34f05acb278c95f2d17bd37fb73e5
Hamim <hamimsiddiq915@gmail.com>
2026-07-01 19:37:47 +0600

    loading

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,11 @@ export const metadata: Metadata = {
   title: "Blog",
 }
 
-export default function Blog() {
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Intentional delay for 3 seconds");
+    }, 3000)
+  });
   return <h1>Blog page</h1>;
 }
