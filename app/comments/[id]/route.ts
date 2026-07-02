2a2053f22820457a030015ecbfb727272c694120
Hamim <hamimsiddiq915@gmail.com>
2026-07-02 20:56:48 +0600

    Handling PATCH Requests

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -4,7 +4,23 @@ export async function GET(
   _request: Request,
   { params }: { params: Promise<{ id: string }> },
 ) {
+  const { id } = await params;
+  const comment = comments.find((c) => c.id === parseInt(id));
+  return Response.json(comment || { error: "Comment not found" }, {
+    status: comment ? 200 : 404,
+  });
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
     const { id } = await params;
-    const comment = comments.find((c) => c.id === parseInt(id));
-    return Response.json(comment || { error: "Comment not found" }, { status: comment ? 200 : 404 });
+    const body = await request.json();
+    const {text} = body;
+
+    const commentIndex = comments.findIndex((c) => c.id === parseInt(id));
+    comments[commentIndex].text = text;
+
+    return Response.json(comments[commentIndex]);
 }
