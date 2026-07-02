ff6731ad5635a8ba0b69b894aa2700c84d800f6f
Hamim <hamimsiddiq915@gmail.com>
2026-07-02 13:55:33 +0600

    parallel routes

---
+++ app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,8 @@
+export default function UserAnalytics() {
+    return (
+        <div>
+            <h1>User Analytics</h1>
+            <p>This is the user analytics page.</p>
+        </div>
+    );
+}
