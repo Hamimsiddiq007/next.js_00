346bb0ce2d881f58ffb1095e74c0914aba79238b
Hamim <hamimsiddiq915@gmail.com>
2026-07-02 23:23:42 +0600

    Cookies in Route Handlers

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,9 +1,20 @@
 import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
     const requestHeader = new Headers(request.headers);
-
     console.log(requestHeader.get("Authorization"));
 
-    return new Response("Hello from the profile API route!");
+    const headersList = await headers();
+    console.log(headersList.get("Authorization"));
+
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+
+    return new Response("<h1>Hello from the API route!</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+            "set-cookie": "theme=dark"
+        },
+    });
 }
