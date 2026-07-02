ad9eeeb00c10c0727ba40c6f49d4ce0ff25b411e
Hamim <hamimsiddiq915@gmail.com>
2026-07-02 23:15:01 +0600

    Headers in Route Handlers

---
+++ app/profile/api/route.ts
@@ -0,0 +1,9 @@
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const requestHeader = new Headers(request.headers);
+
+    console.log(requestHeader.get("Authorization"));
+
+    return new Response("Hello from the profile API route!");
+}
